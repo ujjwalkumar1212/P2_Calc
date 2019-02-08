7b404ac7ee5ca68f19698e4498184bdb7f4d71db
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-08 17:06:53 -0600

    Added test scripts and tested the function

---
+++ src/Web/Contents/Multiplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Contents
+{
+    public static class Multiplier
+    {
+        public static double Multiply( double n1, double n2)
+        {
+            return (n1 * n2);
+        }
+    }
+}
--- tests/Web.Test/UnitTest1.cs
+++ tests/Web.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Web.Contents;
 
 namespace Web.Test
 {
@@ -8,7 +9,21 @@ namespace Web.Test
         [Fact]
         public void Test1()
         {
+            double expected = 12.5;
+            double actual = Multiplier.Multiply(1.25, 10);
+            Assert.Equal(expected, actual);
+        }
 
+        [Theory]
+        [InlineData(-3, 5, -15)]
+        [InlineData(-6, -10, 60)]
+        [InlineData(5, 8, 40)]
+        [InlineData(6, 3, 18)]
+        public void MultiplyTest_SimpleMultiplication( double n1, double n2, double n3)
+        {
+            double expected = n3;
+            double actual = Multiplier.Multiply(n1, n2);
+            Assert.Equal(expected, actual);
         }
     }
 }
