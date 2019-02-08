08fc734fd90ba64dd23d89425206983ed5dd6051
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-08 16:38:58 -0600

    Added the new controller and new methods for the same

---
+++ src/Web/Controllers/NewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+
+namespace Web.Controllers
+{
+    public class New : Controller
+    {
+        public IActionResult Index()
+        {
+            var Rice = new Food { foodName = "Rice" };
+            return View(Rice);
+        }
+
+        public IActionResult TextOutput()
+        {
+            return Content("The Food was awesome!");
+        }
+
+        public IActionResult JsonOutput()
+        {
+            return Json(new
+            {
+                foodName = "Sandwich",
+                foodPrice = 5.23
+            });
+        }
+
+        public IActionResult HTMLOutput()
+        {
+            return Content("<!DOCTYPE html><html><body>I liked Food</body></html>", "text/html");
+        }
+
+        public IActionResult ViewOutput()
+        {
+            var Food = new Food { foodName = "Sandwich" };
+            return View(Food);
+        }
+    }
+}
